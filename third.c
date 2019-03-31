74987160329ebcf079446f3cb668be9d7dcc3dca
Usama Sajid <usanasm@yahoo.com>
2019-03-31 18:12:22 -0400

    ll hashmap

--- third.c
+++ third.c
@@ -7,90 +7,113 @@
 #define size 10000
 
 /*
-****************************************
-****************************************
-              / HashTable\
-****************************************
-****************************************
-                                        */
+ ****************************************
+ ****************************************
+ / HashTable\
+ ****************************************
+ ****************************************
+ */
 
 /*still*/
 
 
 struct node
 {
-	int data;
-	char * s;
-	struct node *next;
-	
+  int data;
+  char * s;
+  struct node *next;
+
 };
 
 struct node * hashtable[size];
-
-void  insert(struct node *head,  char* str,int key)
+struct node* head;
+struct node*  insert(struct node *head,  char* str, int key)
 {
-	 if(head==NULL)
-	 {
-	 	head=(struct node *)malloc(sizeof(struct node));
-	 	head->data=1;
-		head -> s = str;
-	 	head->next=NULL;
-	 	//printf("inserted\n");
-	 	hashtable[key]=head;
-                return;
-	 }
+
+  if(head==NULL)
+  {
+    head=(struct node *)malloc(sizeof(struct node));
+    head->data=1;
+    head -> s = str;
+    head->next=NULL;
+    //printf("inserted\n");
+    //hashtable[key]=head;
+    return head;
+  }
 
 
 
 
-	struct node *temp;
-	temp=head;
+  struct node *temp;
+  temp=head;
 
 
 
-	while(temp->next!=NULL)
-	{
-		if(temp->s==str)
-		{
-			temp->data = (temp->data)+1;
-			return;
-		}
-			temp=temp->next;
-	}
-	    struct node *nxt=(struct node *)malloc(sizeof(struct node));
-	    nxt->data=1;
-		nxt->s = str;
-	    nxt->next=NULL;
-	    temp->next = nxt;
-            return;
-	
+  while(temp->next!=NULL)
+  {
+    if(temp->s==str)
+    {
+      temp->data = (temp->data)+1;
+      return;
+    }
+    temp=temp->next;
+  }
+  struct node *nxt=(struct node *)malloc(sizeof(struct node));
+  nxt->data=1;
+  nxt->s = str;
+  nxt->next=NULL;
+  temp->next = nxt;
+  return head;
+
 }
 
 
 int search(struct node* head, char * str)
 {
-	struct node* temp;
-	temp=head;	
-	//printf("%d\n", data);
+  struct node* temp;
+  temp=head;	
+  //printf("%d\n", data);
 
-	if(temp==NULL)
-	{
-		//printf("absent\n");
-		return -1;
-	}
-   
-   	while(temp!=NULL)
-	{   
-		if(temp->s==str)
-		{
-			//printf("present\n");
-			return temp->data;
-		}
-		//printf("searching %d temp %d\n", data, temp->data );
-		temp=temp->next;
-	}
+  if(temp==NULL)
+  {
+    //printf("absent\n");
+    return -1;
+  }
 
-	//broke out and didnt find
-	//printf("absent\n");
-	return -1;
+  while(temp!=NULL)
+  {   
+    if(temp->s==str)
+    {
+      //printf("present\n");
+      return temp->data;
+    }
+    //printf("searching %d temp %d\n", data, temp->data );
+    temp=temp->next;
+  }
+
+  //broke out and didnt find
+  //printf("absent\n");
+  return -1;
+}
+
+int main()
+{
+
+  head = NULL;
+  head = insert(head, "hello", 0);
+  head = insert(head, "there", 0);
+  head = insert(head, "this", 0);
+  head = insert(head, "hello", 0);
+  int i=0;
+  struct node* temp = head;
+  while(temp!=NULL)
+  {
+    printf("String:%s data:%d\n", temp->s, temp->data);
+    temp = temp->next;
+  }
+
+
+
+
+
 }
